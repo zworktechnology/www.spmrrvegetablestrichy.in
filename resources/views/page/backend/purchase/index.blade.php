a995bfc18eb122d4fa9ec2f4becdd040bfb47d08
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-06-08 19:40:50 +0530

    Main

--- resources/views/page/backend/purchase/index.blade.php
+++ resources/views/page/backend/purchase/index.blade.php
@@ -7,26 +7,18 @@
                 <h4>Purchase</h4>
             </div>
             <div class="page-btn">
-            <a href="{{ route('purchase.create') }}" class="btn btn-added">Add Purchase</a>
+                <a href="{{ route('purchase.create') }}" class="btn btn-added">Add Purchase</a>
             </div>
         </div>
 
 
         <div class="row">
-        @foreach ($allbranch as $keydata => $allbranches)
-            <div class="col-lg-3 col-sm-6 col-12">
-                <div class="dash-widget">
-                    <div class="dash-widgetimg">
-                        <span><img src="{{ asset('assets/backend/img/icons/dash1.svg') }}" alt="img"></span>
-                    </div>
-                    <div class="dash-widgetcontent">
-                        <h5><a href="" style="color: black">{{ $allbranches->name }}</a></h5>
-                        <h6></h6>
-                    </div>
-                </div>
+            <div style="display: flex; margin-bottom: 10px;">
+                <a href="" style="color: white; margin-left: 10px; " class="badges bg-lightyellow">All</a>
+                @foreach ($allbranch as $keydata => $allbranches)
+                    <a href="" style="color: white; margin-left: 10px; " class="badges bg-lightyellow">{{ $allbranches->name }}</a>
+                @endforeach
             </div>
-         @endforeach  
-            
         </div>
 
         <div class="card">
@@ -47,7 +39,8 @@
                             @foreach ($purchase_data as $keydata => $purchasedata)
                                 <tr>
                                     <td>#{{ $purchasedata['bill_no'] }}</td>
-                                    <td>{{ date('d M Y', strtotime($purchasedata['date'])) }} - {{ date('h:i A', strtotime($purchasedata['date'])) }}</td>
+                                    <td>{{ date('d M Y', strtotime($purchasedata['date'])) }} -
+                                        {{ date('h:i A', strtotime($purchasedata['date'])) }}</td>
                                     <td>{{ $purchasedata['supplier_name'] }}</td>
                                     <td>{{ $purchasedata['branch_name'] }}</td>
                                     <td>{{ $purchasedata['gross_amount'] }}</td>
@@ -55,18 +48,22 @@
                                         <ul class="list-unstyled hstack gap-1 mb-0">
                                             <li>
 
-                                                <a href="{{ route('purchase.edit', ['unique_key' => $purchasedata['unique_key']]) }}" class="badges bg-lightyellow" style="color: white">Edit</a>
+                                                <a href="{{ route('purchase.edit', ['unique_key' => $purchasedata['unique_key']]) }}"
+                                                    class="badges bg-lightyellow" style="color: white">Edit</a>
                                             </li>
                                             <li hidden>
                                                 <a href="#delete{{ $purchasedata['unique_key'] }}" data-bs-toggle="modal"
                                                     data-id="{{ $purchasedata['unique_key'] }}"
-                                                    data-bs-target=".purchasedelete-modal-xl{{ $purchasedata['unique_key'] }}" class="badges bg-lightgrey" style="color: white">Delete</a>
+                                                    data-bs-target=".purchasedelete-modal-xl{{ $purchasedata['unique_key'] }}"
+                                                    class="badges bg-lightgrey" style="color: white">Delete</a>
                                             </li>
                                             <li>
-                                                <a href="{{ route('purchase.view', ['unique_key' => $purchasedata['unique_key']]) }}" class="badges bg-lightred" style="color: white">View</a>
+                                                <a href="{{ route('purchase.view', ['unique_key' => $purchasedata['unique_key']]) }}"
+                                                    class="badges bg-lightred" style="color: white">View</a>
                                             </li>
                                             <li>
-                                                <a href="{{ route('purchase.invoice', ['unique_key' => $purchasedata['unique_key']]) }}" class="badges bg-lightgreen" style="color: white">Invoice</a>
+                                                <a href="{{ route('purchase.invoice', ['unique_key' => $purchasedata['unique_key']]) }}"
+                                                    class="badges bg-lightgreen" style="color: white">Invoice</a>
                                             </li>
                                         </ul>
                                     </td>
@@ -87,7 +84,7 @@
             </div>
         </div>
 
-        
+
 
 
     </div>
