2a9f58bc62bccc56ffbc8b087c4bffdfbe0a91f0
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-06-22 18:26:00 +0530

    BANK COMPLETED

--- resources/views/page/backend/bank/delete.blade.php
+++ resources/views/page/backend/bank/delete.blade.php
@@ -1,14 +1,12 @@
 <div class="modal-dialog modal-dialog-centered modal-sm">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title"
-                id="bankdeleteLargeModalLabel{{ $bankdata->unique_key }}">
-                Destroy Unit</h5>
+            <h5 class="modal-title" id="bankdeleteLargeModalLabel{{ $bankdata->unique_key }}">
+                Destroy Bank</h5>
         </div>
         <div class="modal-body px-4 py-5 text-center">
             <div class="avatar-sm mb-4 mx-auto">
-                <div
-                    class="avatar-title bg-primary text-primary bg-opacity-10 font-size-20 rounded-3">
+                <div class="avatar-title bg-primary text-primary bg-opacity-10 font-size-20 rounded-3">
                     <i class="mdi mdi-trash-can-outline"></i>
                 </div>
             </div>
@@ -16,14 +14,11 @@
                 remove the Bank Details.</p>
 
             <div class="hstack gap-2 justify-content-center mb-0">
-                <form autocomplete="off" method="POST"
-                    action="{{ route('bank.delete', [$bankdata->unique_key]) }}">
+                <form autocomplete="off" method="POST" action="{{ route('bank.delete', [$bankdata->unique_key]) }}">
                     @method('PUT')
                     @csrf
                     <button type="submit" class="btn btn-danger">Yes, Delete it</button>
-                </form>
-                <a href="{{ route('bank.index') }}"><button type="button" class="btn btn-secondary"
-                    data-bs-dismiss="modal">Cancel</button></a>
+                </form><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
             </div>
         </div>
     </div>
