c0ce3cf9220645a547de84a3db9073309757a459
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-06-22 18:52:18 +0530

    STOCK COMPLETED

--- resources/views/page/backend/product/stockmanagement.blade.php
+++ resources/views/page/backend/product/stockmanagement.blade.php
@@ -56,8 +56,6 @@
                                           <th>Product</th>
                                           <th>Bags</th>
                                           <th>Kilograms</th>
-                                          <th>Status</th>
-                                          <th>Action</th>
                                     </tr>
                                  </thead>
                                  <tbody>
@@ -68,39 +66,7 @@
                                              <td>{{ $producttdata['productlist'] }}</td>
                                              <td>{{ $producttdata['available_stockin_bag'] }}</td>
                                              <td>{{ $producttdata['available_stockin_kilograms'] }}</td>
-                                             @if ($producttdata['status'] == 0)
-                                                <td><span class="badges bg-lightgreen">Active</span></td>
-                                             @else
-                                                <td><span class="badges bg-lightred">De-Active</span></td>
-                                             @endif
-                                             <td>
-                                                <ul class="list-unstyled hstack gap-1 mb-0">
-                                                      <li>
-                                                         <a href="#edit{{ $producttdata['unique_key'] }}" data-bs-toggle="modal"
-                                                            data-id="{{ $producttdata['unique_key'] }}"
-                                                            data-bs-target=".productedit-modal-xl{{ $producttdata['unique_key'] }}" class="badges bg-lightyellow" style="color: white">Edit</a>
-                                                      </li>
-                                                      <li>
-                                                         <a href="#delete{{ $producttdata['unique_key'] }}" data-bs-toggle="modal"
-                                                            data-id="{{ $producttdata['unique_key'] }}"
-                                                            data-bs-target=".productdelete-modal-xl{{ $producttdata['unique_key'] }}" class="badges bg-lightgrey" style="color: white">Delete</a>
-                                                      </li>
-                                                </ul>
-                                             </td>
                                           </tr>
-
-                                          <div class="modal fade productedit-modal-xl{{ $producttdata['unique_key'] }}" tabindex="-1"
-                                             role="dialog" data-bs-backdrop="static" aria-labelledby="producteditLargeModalLabel{{ $producttdata['unique_key'] }}"
-                                             aria-hidden="true">
-                                             @include('page.backend.product.edit')
-                                          </div>
-
-                                          <div class="modal fade productdelete-modal-xl{{ $producttdata['unique_key'] }}"
-                                             tabindex="-1" data-bs-backdrop="static" role="dialog"
-                                             aria-labelledby="productdeleteLargeModalLabel{{ $producttdata['unique_key'] }}"
-                                             aria-hidden="true">
-                                             @include('page.backend.product.delete')
-                                          </div>
                                     @endforeach
                                  </tbody>
                               </table>
