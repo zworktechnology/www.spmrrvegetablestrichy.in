bb4e1cf5bce7d257ba0576453532b4d81eb2872d
jasperjerome <jasperjerome4@gmail.com>
2023-07-02 17:27:06 +0530

    main

--- resources/views/page/backend/branch/delete.blade.php
+++ resources/views/page/backend/branch/delete.blade.php
@@ -6,14 +6,8 @@
                 Destroy Branch</h5>
         </div>
         <div class="modal-body px-4 py-5 text-center">
-            <div class="avatar-sm mb-4 mx-auto">
-                <div
-                    class="avatar-title bg-primary text-primary bg-opacity-10 font-size-20 rounded-3">
-                    <i class="mdi mdi-trash-can-outline"></i>
-                </div>
-            </div>
-            <p class="text-muted font-size-16 mb-4">Please confirm that you wish to
-                remove the branch.</p>
+            <h5>Are your Sure ?</h5>
+            <p class="text-muted font-size-16 mb-4">You won't be able to revert this!</p>
 
             <div class="hstack gap-2 justify-content-center mb-0">
                 <form autocomplete="off" method="POST"
