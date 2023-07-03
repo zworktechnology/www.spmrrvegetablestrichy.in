d9daeea943950e1c4175bf6f3dad24a54fa9a379
BenittaArun <benitta.jagannathan@gmail.com>
2023-07-03 17:40:43 +0530

    Main

--- resources/views/page/backend/purchase/todaystock.blade.php
+++ resources/views/page/backend/purchase/todaystock.blade.php
@@ -39,10 +39,10 @@
                               </div>
                              
                               <div class="col-lg-3 col-sm-3 col-3 border">
-                                 <span class="" style="vertical-align: inherit;vertical-align: inherit;font-size: 14px;color:#000;font-weight: 700;line-height: 35px; ">{{$p_datas['Sales_Customer']}}</span>
+                                 <span class="" style="vertical-align: inherit;vertical-align: inherit;font-size: 14px;color:#000;font-weight: 700;line-height: 35px; text-transform: uppercase;">{{$p_datas['Sales_Customer']}}</span>
                               </div>
                               <div class="col-lg-4 col-sm-4 col-4 border">
-                                 <span class=""style="vertical-align: inherit;vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;line-height: 35px; ">{{$p_datas['product_name']}}</span>
+                                 <span class=""style="vertical-align: inherit;vertical-align: inherit;font-size: 14px;color:#000;font-weight: 400;line-height: 35px; text-transform: uppercase;">{{$p_datas['product_name']}}</span>
                               </div>
                              <div class="col-lg-2 col-sm-2 col-2 border">
                                 <span class="" style="vertical-align: inherit;vertical-align: inherit;font-size: 14px;color:#000;font-weight: 700;line-height: 35px; ">{{$p_datas['product_count']}} {{$p_datas['bag_kg']}}</span>
