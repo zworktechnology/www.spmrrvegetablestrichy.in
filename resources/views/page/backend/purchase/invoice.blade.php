139798d8bab16741398363b69e7ffca34bd6fa07
BenittaArun <benitta.jagannathan@gmail.com>
2024-01-23 14:20:09 +0530

    Main

--- resources/views/page/backend/purchase/invoice.blade.php
+++ resources/views/page/backend/purchase/invoice.blade.php
@@ -233,7 +233,7 @@
                     </div>
                     <br /><br />
                     <div class="modal-footer">
-                        <input type="submit" class="btn btn-primary" onclick="purchaseinvoiceubmitForm(this);" />
+                        <input type="submit" class="btn btn-primary"  />
                         <a href="{{ route('purchase.index') }}" class="btn btn-danger" value="">Cancel</a>
                     </div>
                 </form>
